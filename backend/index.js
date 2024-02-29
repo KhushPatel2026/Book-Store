42f07aff0546c928daed3a10b20a0e18a006417e
KhushPatel2026 <khushspatel@gmail.com>
2024-02-29 16:02:52 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
 
 const PORT = 5555;
-
+const DB_URL = process.env.MONGODB_URL;
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -21,6 +21,6 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-mongoose.connect('mongodb+srv://khushpatel3482:Mk0eWBkV9cC1mDS5@cluster0.lqykly9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect()
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
